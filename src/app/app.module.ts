7366a270b23f7645b05ce869f28e673ae99c5134
Chiara <brichot.chiara@yahoo.fr>
2022-03-11 03:39:06 +0100

    :sparkles: : first commit (component header footer index faq)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { FaqComponent } from './faq/faq.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    FaqComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
